5c468b39e01fcbf518db4c50480b65df34134ffc
Deepak Singh Porte <100459469+deepakk177@users.noreply.github.com>
2026-02-13 13:43:27 +0530

    Delete gesture-image-sphere directory

--- gesture-image-sphere/postcss.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {}, // Fixes user request "Fixing CSS Property Order" somewhat but mainly for vendor prefixes
-    },
-}
