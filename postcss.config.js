de697e4f9bbc7145e83f0e557e9c665b24a00935
Deepak Singh Porte <100459469+deepakk177@users.noreply.github.com>
2026-02-13 13:45:10 +0530

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {}, // Fixes user request "Fixing CSS Property Order" somewhat but mainly for vendor prefixes
+    },
+}
